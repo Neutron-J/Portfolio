c52e09c95d63d31122172e7bdc76e5b15d78a790
James Bates <jamesbates@Jamess-MacBook-Pro-2.local>
2021-08-27 18:24:19 +0100

    Init commit

---
+++ components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Bates</h1>
+</template>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="full-width grid grid-cols-1 md:grid-cols-2">
+    <section>
+      <Header />
+    </section>
+    <section>
+      <h2>Some content</h2>
+    </section>
+  </div>
+</template>
+
+<script>
+
+</script>
